e55f4f1e7a65eb0259bb59a135eb7fa7d13335f4
Tal <talsh838@gmail.com>
2022-05-25 15:14:43 +0300

    link Guide and Dash

--- app/src/main/java/com/srd/srdapplication/Guide.java
+++ app/src/main/java/com/srd/srdapplication/Guide.java
@@ -1,4 +1,26 @@
 package com.srd.srdapplication;
 
-public class Guide {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Guide extends AppCompatActivity {
+    //Button mDas;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+        //mDas = findViewById(R.id.button2);
+
+
+//        mDas.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(getApplicationContext(),Dashborad.class));
+//            }
+//        });
+    }
 }
--- app/src/main/java/com/srd/srdapplication/Intro.java
+++ app/src/main/java/com/srd/srdapplication/Intro.java
@@ -1,7 +1,35 @@
 package com.srd.srdapplication;
 
-public class Intro {
-    String dd;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Intro extends AppCompatActivity {
+    Button mDas;
+    Button mGui;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        mDas = findViewById(R.id.button2);
+        mGui = findViewById(R.id.button);
 
 
+        mDas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Dashborad.class));
+            }
+        });
+        mGui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Guide.class));
+            }
+        });
+    }
 }
+
